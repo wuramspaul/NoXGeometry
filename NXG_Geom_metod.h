4e3dcbb3b43c9678d9fd0a0ad2ffb439aa5575aa
wuramspaul <wuramspaul@gmail.com>
2019-11-04 06:44:48 +0300

    refactr

---
+++ NXG_Geom_metod.h
@@ -0,0 +1,6 @@
+#include "StdNXG.h"
+output_t equally(input_t*); // 0 - x
+output_t equally2(input_t* input);  // 0 - x
+output_t equally2y(input_t* input); // 0 - x
+output_t Pifagor_katet(input_t* input); // 0 - gip; 1 - katet1; 2 - katet2   
+output_t SideOfSqere_ForS(input_t* input); // 0 - sqere; 1 - side  
