c8992c3949ab70afc4aa0b734b3c6446c31c6fd7
wuramspaul <wuramspaul@gmail.com>
2019-11-05 20:49:33 +0300

    v0.1

---
+++ src/memu2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ncurses.h>
+#define MAXNAME 255
+struct inputmenu{
+  bool create;
+  bool save;
+  bool load;
+  bool exit;
+  bool retter;
+};
+
+struct outputmes{
+  char create[MAXNAME];
+  
+};
+
+int main() {
+  return 0;
+}
